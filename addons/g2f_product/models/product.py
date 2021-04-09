e87b3bc58cae1cda224db78a7816151f9f9f8389
Boris <silvaboris@gmail.com>
2021-04-08 22:50:34 -0400

    [MOD] Incorporar "Nutrition Facts" en la ficha del producto Parte 3

--- addons/g2f_product/models/product.py
+++ addons/g2f_product/models/product.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from random import randint
 
 
 class ProductTemplate(models.Model):
@@ -22,10 +23,47 @@ class ProductTemplate(models.Model):
     peso_estante = fields.Integer('Peso total del estante (g)', compute='_get_peso_estante')
     aptitud = fields.Integer('Porcentaje de aptitud de vida útil', required=True, default=70)
     desc_tag = fields.Char('Descripción corta del TAG', required=True)
+    atributos_ids = fields.Many2many('product.atributos', 'product_atributos_rel', 'prod_id', 'atributos_id', string='Atributos')
 
     @api.depends('peso_bruto', 'cant_frente', 'cant_fondo', 'cant_altura')
     def _get_peso_estante(self):
         self.peso_estante = self.peso_bruto * self.cant_frente * self.cant_fondo * self.cant_altura
+
+    @api.onchange('sector')
+    def onchange_sector(self):
+        res = {}
+        familia_obj = self.env['product.familia']
+        familia_ids = familia_obj.search([('sector', '=', self.sector.id)])
+        if familia_ids:
+            res['domain'] = {'familia': [('id', 'in', familia_ids.ids)]}
+        else:
+            res['domain'] = {'familia': [('id', '=', 0)]}
+        self.familia = False
+        return res
+
+    @api.onchange('familia')
+    def onchange_familia(self):
+        res = {}
+        subfamilia_obj = self.env['product.subfamilia']
+        subfamilia_ids = subfamilia_obj.search([('familia', '=', self.familia.id)])
+        if subfamilia_ids:
+            res['domain'] = {'subfamilia': [('id', 'in', subfamilia_ids.ids)]}
+        else:
+            res['domain'] = {'subfamilia': [('id', '=', 0)]}
+        self.subfamilia = False
+        return res
+
+    @api.onchange('subfamilia')
+    def onchange_subfamilia(self):
+        res = {}
+        categoria_obj = self.env['product.categoria']
+        categoria_ids = categoria_obj.search([('subfamilia', '=', self.subfamilia.id)])
+        if categoria_ids:
+            res['domain'] = {'categoria': [('id', 'in', categoria_ids.ids)]}
+        else:
+            res['domain'] = {'categoria': [('id', '=', 0)]}
+        self.categoria = False
+        return res
 
 
 class ProductSector(models.Model):
@@ -63,3 +101,17 @@ class ProductCategoria(models.Model):
     subfamilia = fields.Many2one('product.subfamilia', 'Subfamilia')
 
 
+class ProductAtributos(models.Model):
+    _name = 'product.atributos'
+    _description = 'Atributos Nutrition Facts'
+
+    def _get_default_color(self):
+        return randint(1, 11)
+
+    name = fields.Char('Atributos', required=True, translate=True)
+    color = fields.Integer('Color', default=_get_default_color)
+    # product_ids = fields.Many2many('product.template', 'product_atributos_rel', 'atributos_id', 'prod_id', string='Producto')
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', "Ya existe una etiqueta con ese nombre !"),
+    ]
