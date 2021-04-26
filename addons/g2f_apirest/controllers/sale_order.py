3ccab1a3e89dbc96f30ae1f71e945737b4a10448
carlos.garcia <foxcarlos@gmail.com>
2021-04-25 23:50:06 -0300

    feat: endpoint para lista de ordenes del carrito

--- addons/g2f_apirest/controllers/sale_order.py
+++ addons/g2f_apirest/controllers/sale_order.py
@@ -10,7 +10,7 @@ from odoo import http, _
 class SaleOrderCart(http.Controller):
     @http.route(['/user_cart'], type='json', auth='public',
             methods=['GET', 'PUT', 'POST', 'DELETE'], website=True, csrf=False)
-    def sale_order_cart(self, **kw):
+    def user_cart_from_vision(self, **kw):
         method = http.request.httprequest.method
         kw = http.request.jsonrequest
 
@@ -22,16 +22,33 @@ class SaleOrderCart(http.Controller):
 
         if method == 'GET':
             # Obtener lista de productos de orden de venta abierta
-
             response = sale_order.sudo()._get_sale_order_from_controller(
-                    user_id)
+                user_id)
             return dumps(response)
 
         if method == 'POST':
             #  Agregar un prodcuto a una orden de venta
-
             response = sale_order.sudo()._add_products_from_controller(
-                    user_id, product_id, action)
+                user_id, product_id, action)
             if response:
                 return http.Response('CREATED', status=201)
             return http.Response('NOT FOUND', status=404)
+
+    @http.route(['/sale_order_cart'], type='http', auth='public',
+                methods=['GET'], website=True, csrf=False)
+    def sale_order_cart(self, **kw):
+        '''Get sale order.'''
+
+        print(kw)
+        method = http.request.httprequest.method
+        user_id = kw.get('user')
+
+        sale_order = http.request.env['sale.order']
+
+        if method == 'GET':
+            # Obtener lista de productos de orden de venta abierta
+            response = sale_order.sudo()._get_sale_order_from_controller(
+                user_id)
+            return dumps(response)
+
+        return http.Response('NOT FOUND', status=404)
