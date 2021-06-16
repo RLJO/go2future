936b4921470ce732c898cdf73f678bc3db87b598
Boris <silvaboris@gmail.com>
2021-06-15 21:11:05 -0400

    [ADD] Se agrega API Product list

---
+++ addons/g2f_seller_invoice/models/product.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.model
+    def get_product_data(self, vals):
+        domain = [('type', '=', 'product')]
+        data = {"dataList": []}
+        if vals:
+            domain = [
+                ('default_code', '=', vals['sku']),
+                ('type', '=', 'product')
+            ]
+        product_ids = self.search(domain)
+        for product in product_ids:
+            second_line = product.brand + ' ' if product.brand else ''
+            second_line += str(product.contents) + ' ' if product.contents else ''
+            second_line += product.uom_id.name
+            head = {
+                "stationCode": "G2F",
+                "id": product.default_code,
+                "name": product.name,
+                "nfc": "https://minigo-visual-impairment.s3-sa-east-1.amazonaws.com/iTANKRDes.mp3",
+                "originPrice": False,
+                "salePrice": False,
+                "discountPercent": False,
+                "data": {
+                    "ARTICLE_ID": product.default_code,
+                    "ITEM_FIRST_LINE": product.desc_tag,
+                    "ITEM_SECOND_LINE": second_line,
+                    "SALE_PRICE": str(product.list_price),
+                    "LITER_PRICE": "0",
+                    "WEIGHT_UNIT": "0"
+                }
+            }
+            data["dataList"].append(head)
+        if not product_ids:
+            data = 'No se encontró producto registrado con el SKU: %s' % vals['sku']
+        return data
