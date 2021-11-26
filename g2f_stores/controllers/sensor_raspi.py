ad0527f7b98e97951b51f7e22e7178287e378962
Rafael Andara D <randarad@gmail.com>
2021-11-26 15:36:29 -0300

    [ADD] API para consulta de Raspberry (Gondolas) de una tienda (solicitud de Axel)

--- g2f_stores/controllers/sensor_raspi.py
+++ g2f_stores/controllers/sensor_raspi.py
@@ -50,3 +50,27 @@ class StoreRasberryPi(http.Controller):
                 response['error_data'] = 'No data found!'
 
         return response
+
+    @http.route(['/store/get_gondola_store'], type='json', auth='public', methods=['POST'], website=True, csrf=False)
+    def get_gondola_store(self, **kw):
+        method = http.request.httprequest.method
+        store_code = kw.get('store_code')
+        response = {"id": store_code, "status": 200, "data": []}
+        store_id = request.env['stock.warehouse'].sudo().search([("code", "=", store_code)])
+        # make a method
+        if method == "POST":
+            # obj = request.env['store.sensor'].sudo()
+            # res = obj.post_sensor_calibration_data(store)
+            # data = request.env['store.sensor'].sudo().search([("store_id", "=", store_id.id)])
+            data = http.request.env['store.raspi'].sudo().search_read(
+                [("store_id", "=", store_id.id)], fields=['id', 'name', 'ip_add'])
+            if data:
+                response['success'] = True
+                response['data'] = data
+            else:
+                _logger.info("No data found: %s" % response)
+                response['success'] = False
+                response['error_code'] = 1
+                response['error_data'] = 'No data found!'
+
+        return response
