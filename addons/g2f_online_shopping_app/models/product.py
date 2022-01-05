13adfb3c8b5cecdf0369854392010f02d4f8c167
foxcarlos <foxcarlos@gmail.comm>
2022-01-04 21:30:15 -0300

    [feat/g2f_onlline_shopping_app&g2f_product] category parse products

--- addons/g2f_online_shopping_app/models/product.py
+++ addons/g2f_online_shopping_app/models/product.py
@@ -18,10 +18,8 @@ class ProductProduct(models.Model):
 
     _inherit = 'product.product'
 
-    def parse_products(self, domain=None):
-        """Return  products parse."""
-
-        values = super(ProductProduct, self).parse_products(domain)
+    def parse_products_online_shopping(self, domain=[]):
+        """Return  products parse for online shopping."""
 
         search_products = self.search(domain)
         response = [{
@@ -34,8 +32,33 @@ class ProductProduct(models.Model):
             'vegan': p.vegan, 'organic': p.organic, 
             'without_tacc': p.without_tacc, 'sugar_free': p.sugar_free, 
             'alternative_product_ids': [f.name for f in p.alternative_product_ids], 
+            'public_categ_ids': [c.name for c in p.public_categ_ids], 
             'image_1920': p.image_1920.decode("ascii"), 
             'iframe': p.iframe} for p in search_products
             ]
         return response
+
+    def parse_product_public_category(self, domain=[]):
+        """Return parse product public category list."""
+
+        ppcatg = self.env['product.public.category'].search(domain)
+        response = [{
+            'name': f.name,
+            'padre': f.parent_id.name if f.parent_id else ''} 
+            for f in ppcatg]
+
+        print(response)
+
+    def products_for_category_list(self, domain=[]):
+        """Return list product for category."""
+
+        ppcatg = self.env['product.public.category'].search(domain)
+        response = [{
+            'category_id': category.id,
+            'category': category.name,
+            'product_tmpl_ids': 
+            [product.name for product in category.product_tmpl_ids]
+            } 
+            for category in ppcatg.filtered('product_tmpl_ids')]
+        print(response)
 
